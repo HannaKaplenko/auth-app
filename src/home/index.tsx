b4aac87cfcf8f68f2e00eabbd5ceb3b5014c465e
Hanna <erion777a@gmail.com>
2025-05-15 02:36:53 +0300

    first commit

---
+++ src/home/index.tsx
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from 'react';
+import {styles} from './styles';
+import {Text, View, FlatList, Image} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export const Products = () => {
+  const [isLoading, setIsLoading] = useState (true);
+    const [products, setProducts] = useState<any>([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+  
+
+  const getProducts = async () => {
+    const response = await fetch('https://dummyjson.com/products');
+    const data = await response.json();
+    setProducts(data.products);
+  };
+  
+  console.log('Products', products);
+
+  return (
+    <SafeAreaView style={styles.container}>
+  
+      <FlatList
+        data={products}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item}) => (
+          <View style={styles.card}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.price}>{item.price}</Text>
+            <Image source={{ uri: item.images[0] }}  style={styles.image} />
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
+};
